440920a038979c92b7e545a6f547217f06a2f026
BlubbFish <git@blubbfish.net>
2019-03-31 13:39:27 +0200

    add update.sh Update mqtt-map

---
+++ update.sh
@@ -0,0 +1,38 @@
+echo "Switch to subdir Librarys"
+cd Librarys
+echo "Pull litjson"
+cd litjson
+git pull
+cd ..
+echo "Pull Mono.Posix"
+cd Mono.Posix
+git pull
+cd ..
+echo "Pull mqtt"
+cd mqtt
+git pull
+cd ..
+cd ..
+echo "Pull mqtt-map"
+cd mqtt-map
+git pull
+cd ..
+echo "Switch to subdir Utils"
+cd Utils
+echo "Pull Bot-Utils"
+cd Bot-Utils
+git pull
+cd ..
+echo "Pull ConnectorDataMqtt"
+cd ConnectorDataMqtt
+git pull
+cd ..
+echo "Pull Utils"
+cd Utils
+git pull
+cd ..
+echo "Pull Utils-IoT"
+cd Utils-IoT
+git pull
+cd ..
+read -p "Press return"
