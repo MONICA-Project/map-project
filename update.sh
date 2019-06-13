c75aae3d96442eb77e9fe139b308dcd31c7dabe1
Philip Schell <philip.schell@fit.fraunhofer.de>
2019-06-13 11:11:24 +0200

    Merge createci to master with .gitlab-ci.yml and update.sh Update to last Lora-Map

--- update.sh
+++ update.sh
@@ -1,38 +1,45 @@
+git submodule update --init --  Librarys/litjson
+git submodule update --init --  Librarys/Mono.Posix
+git submodule update --init --  Librarys/mqtt
+git submodule update --init --  Utils/Bot-Utils
+git submodule update --init --  Utils/ConnectorDataMqtt
+git submodule update --init --  Utils/Utils
+git submodule update --init --  Utils/Utils-IoT
 echo "Switch to subdir Librarys"
 cd Librarys
 echo "Pull litjson"
 cd litjson
-git pull
+git pull origin master
 cd ..
 echo "Pull Mono.Posix"
 cd Mono.Posix
-git pull
+git pull origin master
 cd ..
 echo "Pull mqtt"
 cd mqtt
-git pull
+git pull origin master
 cd ..
 cd ..
 echo "Pull Lora-Map"
 cd Lora-Map
-git pull
+git pull origin master
 cd ..
 echo "Switch to subdir Utils"
 cd Utils
 echo "Pull Bot-Utils"
 cd Bot-Utils
-git pull
+git pull origin master
 cd ..
 echo "Pull ConnectorDataMqtt"
 cd ConnectorDataMqtt
-git pull
+git pull origin master
 cd ..
 echo "Pull Utils"
 cd Utils
-git pull
+git pull origin master
 cd ..
 echo "Pull Utils-IoT"
 cd Utils-IoT
-git pull
+git pull origin master
 cd ..
 read -p "Press return"
